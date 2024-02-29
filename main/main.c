c4623e1296209870bc47d97e74f5e725ac580d3a
HenriqueFBadin <henrique.badin@gmail.com>
2024-02-29 21:54:18 +0000

    Atualizando lógica do jogo

--- main/main.c
+++ main/main.c
@@ -4,27 +4,107 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include <time.h>
+#include <stdlib.h>
 
 const int BTN_PIN_B = 16;
-const int BTN_PIN_G = 21;
+const int BTN_PIN_G = 17;
+const int BTN_PIN_R = 13;
+const int BTN_PIN_Y = 12;
 const int LED_B = 18;
-const int LED_G = 19; 
+const int LED_G = 19;
+const int LED_R = 20;
+const int LED_Y = 22; 
 const int BUZZER = 15;
 volatile int blue_flag = 0;
 volatile int green_flag = 0;
+volatile int red_flag = 0;
+volatile int yellow_flag = 0;
+volatile int blue = 0;
+volatile int green = 0;
+volatile int red = 0;
+volatile int yellow = 0;
+
+#define DEBOUNCE_DELAY_MS 50
+
+volatile uint32_t last_button_press_time = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-    if (gpio == BTN_PIN_B) {
-      blue_flag = 1;
+    // Get the current time
+    uint32_t current_time = to_ms_since_boot(get_absolute_time());
+
+    // Check if it's been long enough since the last button press
+    if (current_time - last_button_press_time < DEBOUNCE_DELAY_MS) {
+        // If it hasn't been long enough, ignore this button press
+        return;
     }
-    else if (gpio == BTN_PIN_G) {
-      green_flag = 1;
+
+    // Record the time of this button press
+    last_button_press_time = current_time;
+
+    // Handle the button press based on the GPIO and events
+    if (events == 0x4) { // Falling edge
+        if (gpio == BTN_PIN_B) {
+            blue_flag = 1;
+        }
+        else if (gpio == BTN_PIN_G) {
+            green_flag = 1;
+        }
+        else if (gpio == BTN_PIN_R) {
+            red_flag = 1;
+        }
+        else if (gpio == BTN_PIN_Y) {
+            yellow_flag = 1;
+        }
     }
-  }
+}
+
+
+void generateSequence(int sequence[], int length) {
+    srand(time(NULL));
+
+    for (int i = 0; i < length; i++) {
+        sequence[i] = rand() % 4;
+    }
+}
+
+void displayColor(int color) {
+    if (color == 0) 
+    {
+      gpio_put(LED_B, 1);
+      sleep_ms(500);
+      gpio_put(LED_B, 0);
+      sleep_ms(500);
+      blue += 1;
+    }
+    else if (color == 1) 
+    {
+      gpio_put(LED_G, 1);
+      sleep_ms(500);
+      gpio_put(LED_G, 0);
+      sleep_ms(500);
+      green += 1;
+    }
+    else if (color == 2) 
+    {
+      gpio_put(LED_R, 1);
+      sleep_ms(500);
+      gpio_put(LED_R, 0);
+      sleep_ms(500);
+      red += 1;
+    }
+    else if (color == 3) 
+    {
+      gpio_put(LED_Y, 1);
+      sleep_ms(500);
+      gpio_put(LED_Y, 0);
+      sleep_ms(500);
+      yellow += 1;
+    }
 }
 
 void play(int freq, int tempo, int pino) {
@@ -37,9 +117,45 @@ void play(int freq, int tempo, int pino) {
     }
 }
 
+void play_victory_theme(int pino) {
+  int level_complete_notes[] = {659, 659, 659, 523, 659, 523, 659, 440, 523, 494, 523, 440};
+  int level_complete_durations[] = {125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125};
+
+  for (int i = 0; i < sizeof(level_complete_notes) / sizeof(level_complete_notes[0]); i++) {
+    play(level_complete_notes[i], level_complete_durations[i], pino);
+    sleep_ms(50); // Pause between notes
+  }
+}
+
+void play_loss_theme(int pino) {
+  int level_complete_notes[] = {659, 659, 659, 659, 523, 523, 587, 659, 523, 440, 370, 440};
+  int level_complete_durations[] = {125, 125, 125, 125, 125, 125, 250, 125, 62, 125, 125, 125};
+
+  for (int i = 0; i < sizeof(level_complete_notes) / sizeof(level_complete_notes[0]); i++) {
+    play(level_complete_notes[i], level_complete_durations[i], pino);
+    sleep_ms(50); // Pause between notes
+  }
+}
+
+int igualNoIntervalo(int v[], int v2[], int l, int r) {
+    if (l == r) {
+        return 1;
+    }
+
+    if (v[l] != v2[l]) {
+        return 0;
+    }
+
+    return igualNoIntervalo(v, v2, l + 1, r);
+}
+
 int main() {
-  int azul = 0;
   int play_once = 0;
+  int answer[16];
+  int player_answer[16];
+  int round = 1;
+  int count = 0;
+
   stdio_init_all();
 
   gpio_init(BUZZER);
@@ -51,6 +167,12 @@ int main() {
   gpio_init(LED_G);
   gpio_set_dir(LED_G, GPIO_OUT);
 
+  gpio_init(LED_R);
+  gpio_set_dir(LED_R, GPIO_OUT);
+
+  gpio_init(LED_Y);
+  gpio_set_dir(LED_Y, GPIO_OUT);
+
   gpio_init(BTN_PIN_B);
   gpio_set_dir(BTN_PIN_B, GPIO_IN);
   gpio_pull_up(BTN_PIN_B);
@@ -59,44 +181,109 @@ int main() {
   gpio_set_dir(BTN_PIN_G, GPIO_IN);
   gpio_pull_up(BTN_PIN_G);
 
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(BTN_PIN_Y);
+  gpio_set_dir(BTN_PIN_Y, GPIO_IN);
+  gpio_pull_up(BTN_PIN_Y);
+
   // callback led r (first)
   gpio_set_irq_enabled_with_callback(BTN_PIN_B, GPIO_IRQ_EDGE_FALL, true,
                                       &btn_callback);
 
   // callback led g (nao usar _with_callback)
   gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
+  gpio_set_irq_enabled(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true);
+  gpio_set_irq_enabled(BTN_PIN_Y, GPIO_IRQ_EDGE_FALL, true);
+
+  generateSequence(answer, 16);
 
   while (true) {
     if (play_once == 0)
     {
-      gpio_put(LED_B, 1);
-      sleep_ms(500);
-      gpio_put(LED_B, 0);
-      sleep_ms(500);
-      azul = 1;
+      for (int i = 0; i < round; i++)
+      {
+        displayColor(answer[i]);
+      }
+
       play_once = 1;
     }
     
-    if (blue_flag)
-    {
-        sleep_ms(1);
-        play(440, 900, BUZZER);       
+    if (blue_flag) {
+      sleep_ms(1);
+      play(440, 100, BUZZER);
+      blue_flag = 0;
+      player_answer[count] = 0;
+      count += 1;
+    }
+    if (green_flag) {
+      sleep_ms(1);
+      play(392, 100, BUZZER);
+      green_flag = 0;
+      player_answer[count] = 1;
+      count += 1;
+    }
+    if (red_flag) {
+      sleep_ms(1);
+      play(262, 100, BUZZER);
+      red_flag = 0;
+      player_answer[count] = 2;
+      count += 1;
+    }
+    if (yellow_flag) {
+      sleep_ms(1);
+      play(294, 100, BUZZER);
+      yellow_flag = 0;
+      player_answer[count] = 3;
+      count += 1;
     }
 
-    if (azul && blue_flag) 
+    if (igualNoIntervalo(answer, player_answer, 0, count) == 1 && round == count)
     {
-      gpio_put(LED_G, 1);
-      sleep_ms(500);
-      gpio_put(LED_G, 0);
-      blue_flag = 0;
+      for (int i = 0; i < 3; i++) 
+      {
+        gpio_put(LED_G, 1);
+        sleep_ms(500);
+        gpio_put(LED_G, 0);
+        sleep_ms(500);
+      }
+      play_victory_theme(BUZZER);
+
+      round += 1;      
+      count = 0;
+      blue = 0;
+      green = 0;
+      red = 0;
+      yellow = 0;
       play_once = 0;
+      for (int i = 0; i < 16; i++) {
+        player_answer[i] = 5;
+      }
     }
-    
-    if (green_flag)
+
+    else if (round < count || player_answer[round-1] < 4)    
     {
-        green_flag = 0;
-        sleep_ms(1);
-        play(330, 300, BUZZER);          
+      for (int i = 0; i < 3; i++) 
+      {
+        gpio_put(LED_R, 1);
+        sleep_ms(500);
+        gpio_put(LED_R, 0);
+        sleep_ms(500);
+      }
+      play_loss_theme(BUZZER);
+
+      round = 1;
+      count = 0;
+      blue = 0;
+      green = 0;
+      red = 0;
+      yellow = 0;
+      play_once = 0;
+      for (int i = 0; i < 16; i++) {
+        player_answer[i] = 5;
+      }
     }
   }
 }
