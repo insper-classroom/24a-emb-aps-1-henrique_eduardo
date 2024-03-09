b37a08cf96b0b0786ada7e0d9c6f62f5283de0e1
EduSelber <eduardosc6@al.insper.edu.br>
2024-03-09 15:31:50 +0000

    Corrigi o btn_callback

--- main/main.c
+++ main/main.c
@@ -64,11 +64,10 @@ void btn_callback(uint gpio, uint32_t events) {
         else if (gpio == BTN_PIN_START) {
             flag_start= 1;
         }
-        else if(events ==0x8){
+    }else if(events == 0x8){
           if (gpio == BTN_PIN_R){
             flag_start = 0;
           }
-        }
     }
 }
 
